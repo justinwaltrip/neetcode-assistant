3727ae2aa6821affce1418691863402974d77adb
Justin Waltrip <justin.waltrip@protonmail.com>
2024-02-17 17:28:44 -0500

    Remove installation of youtube-dl

--- install.sh
+++ install.sh
@@ -5,7 +5,3 @@ source .venv/bin/activate
 # install python packages
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
-
-# install youtube-dl
-sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
