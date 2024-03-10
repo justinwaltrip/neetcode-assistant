36d5296702b5f725eed90a772be493516260013c
Justin Waltrip <justin.waltrip@protonmail.com>
2024-03-10 18:57:20 -0400

    Add chat functionality to API

--- api/app.py
+++ api/app.py
@@ -1,14 +1,19 @@
 import json
 import os
 import sys
+from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from langchain_community.vectorstores import Milvus
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from pydantic import BaseModel
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-from utils import get_collection_name, get_embeddings
+from utils import format_docs, get_collection_name, get_embeddings, get_llm
 
 PROBLEMS_PATH = "data/processed/index.json"
 
@@ -59,3 +64,90 @@ def post_retrieve(body: RetrieveRequest):
     return vector_db.similarity_search(
         body.query,
     )
+
+
+class ChatRequest(BaseModel):
+    problem_name: str
+    query: str
+    streaming: Optional[bool] = False
+
+
+@app.post("/chat")
+async def post_chat(body: ChatRequest):
+    """Chat with the bot."""
+    # get problem (if exists)
+    try:
+        problem = next(
+            problem for problem in problems if problem["name"] == body.problem_name
+        )
+    except StopIteration:
+        return {"error": "problem not found"}
+
+    # check if problem is indexed
+    if not problem.get("indexed", False):
+        return {"error": "problem not indexed"}
+
+    # connect to vector database
+    vector_db = Milvus(
+        embeddings,
+        collection_name=get_collection_name(body.problem_name),
+    )
+
+    template = """You are helping someone with a LeetCode problem.
+    I will give you the problem description, some relevant sections of the solution transcript, and the problem solution.
+    Use this information to answer the question at the end.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Keep your answer concise.
+    Format your answer using markdown.
+
+    Here is the problem description:
+    <description>
+    {description}
+    </description> 
+
+    Here are some relevant sections of the solution transcript (hidden from the user):
+    <transcript>
+    {context}
+    </transcript>
+
+    Here is the solution (hidden from the user):
+    <solution>
+    {solution}
+    </solution>
+
+    The user does have access to the problem description, but not the solution transcript or the solution.
+    You can use the solution transcript and solution to help answer the user's question, but don't refer to them directly.
+    Your goal is to help the user get to the solution, not to give them the solution directly.
+    Give the user a helpful hint or a nudge in the right direction, but don't give them the answer directly.
+
+    Question: {question}
+
+    Helpful Answer:"""
+    prompt = PromptTemplate.from_template(
+        template,
+        partial_variables={
+            "description": problem["description"],
+            "solution": problem["solution"],
+        },
+    )
+
+    # initialize RAG chain
+    rag_chain = (
+        {
+            "context": vector_db.as_retriever() | format_docs,
+            "question": RunnablePassthrough(),
+        }
+        | prompt
+        | get_llm()
+        | StrOutputParser()
+    )
+
+    if body.streaming:
+        # create a response iterator
+        def stream_response():
+            for chunk in rag_chain.stream(body.query):
+                yield chunk
+
+        return StreamingResponse(stream_response(), media_type="text/plain")
+    else:
+        return rag_chain.invoke(body.query).strip()
--- utils.py
+++ utils.py
@@ -1,10 +1,15 @@
 from langchain_community.embeddings import LocalAIEmbeddings
+from langchain_community.llms import Ollama
 from slugify import slugify
+
+EMBEDDING_MODEL = "text-embedding-ada-002"
+LLM_MODEL = "mixtral:8x7b-instruct-v0.1-q5_K_M"
 
 
 def get_embeddings():
     return LocalAIEmbeddings(
-        openai_api_base="http://localhost:8080", model="text-embedding-ada-002"
+        openai_api_base="http://localhost:8080",
+        model=EMBEDDING_MODEL,
     )
 
 
@@ -13,3 +18,13 @@ def get_collection_name(problem_name):
     if collection_name[0].isdigit():
         collection_name = f"_{collection_name}"
     return collection_name
+
+
+def get_llm():
+    return Ollama(
+        model=LLM_MODEL,
+    )
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
